c56bf91f80b678f91b4b3c3e51963436652fbb9b
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2022-09-13 08:14:08 +0200

    gun counter

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,17 +1,39 @@
-import { useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 import { Button } from "../components/Button.tsx";
+import Gun from "./Gun.mts";
 
 interface CounterProps {
   start: number;
 }
 
 export default function Counter(props: CounterProps) {
+  const gun = Gun.value;
+  const myCount = useRef(gun.get("test").get("counter"));
   const [count, setCount] = useState(props.start);
+
+  useEffect(() => {
+    myCount.current.on((data: number) => {
+      setCount(data);
+    });
+  }, []);
+
   return (
     <div>
       <p>{count}</p>
-      <Button onClick={() => setCount(count - 1)}>-1</Button>
-      <Button onClick={() => setCount(count + 1)}>+1</Button>
+      <Button
+        onClick={() => {
+          myCount.current.put(count - 1);
+        }}
+      >
+        -1
+      </Button>
+      <Button
+        onClick={() => {
+          myCount.current.put(count + 1);
+        }}
+      >
+        +1
+      </Button>
     </div>
   );
 }
